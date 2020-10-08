a062325029b5d4a77a19081d1f27de3c8d078f92
adrianabgonzalez <adrianabgonzalezl@gmail.com>
2020-10-08 19:57:03 -0300

    Primera version del Portafolio

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $("img").click(function(){
+      alert("¡Ooops, no hay nada aquí!");
+    });
+  });
+
+
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+  })
+
+  $(function () {
+    $('[data-toggle="popover"]').popover()
+  })
